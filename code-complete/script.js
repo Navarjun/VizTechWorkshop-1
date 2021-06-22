5f4363e3d8207877eaf66160155c0a03af429014
Navarjun <sfu@sfus-MBP.hitronhub.home>
2021-06-22 16:30:09 -0700

    starter code

---
+++ code-complete/script.js
@@ -0,0 +1,17 @@
+// CIRCLE INTERACTION
+
+let circlesArray = document.getElementsByTagName('circle');
+
+let circle = circlesArray[0];
+
+circle.onclick = function () {
+    circle.setAttribute('r', 100);
+}
+
+// RECTANGLE INTERACTION
+
+let rectanglesArray = document.getElementsByTagName('rect');
+let rect = rectanglesArray[0];
+rect.onclick = function () {
+    rect.style.strokeWidth = '2';
+}
